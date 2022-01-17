3b3031a9127e6993469eb2063d6af7b6c057f018
shadiqshai2000 <84706863+shadiqshai2000@users.noreply.github.com>
2022-01-17 12:00:57 +0530

    Add files via upload
    files added

---
+++ Test_Cron/src/main/java/com/example/Test_Cron/TestCronApplication.java
@@ -0,0 +1,13 @@
+package com.example.Test_Cron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCronApplication.class, args);
+	}
+
+}
---
+++ Test_Cron/src/test/java/com/example/Test_Cron/TestCronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Test_Cron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
