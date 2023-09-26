2ce89afe71790452cd4a5fed0e7766aead3515ef
shadiqshaikh2000 <shadiq.shaik@javis.ai>
2023-09-26 21:43:02 +0530

    Changed log statement

--- Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
+++ Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
@@ -3,7 +3,7 @@ package com.example.Test_Cron;
 public class HoldCron {
 
     public static void main(){
-        System.out.println("This is HoldCron of shadiqshaikiit id");
+        System.out.println("This is main branch");
     }
     
     
