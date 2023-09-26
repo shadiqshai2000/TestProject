d15f55470c9b88dac42270c94d08b7361b597098
shadiqshaikh2000 <shadiq.shaik@javis.ai>
2023-09-26 23:30:58 +0530

    commit-4-uat

--- Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
+++ Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
@@ -5,8 +5,7 @@ public class HoldCron {
     public static void main(){
         System.out.println("This is a commit of main branch");
         System.out.println("This is an extra line added in uat branch");
-        System.out.println("This is an another extra line added in uat branch " +
-                "which is not present in master branch");
+        System.out.println("This is an another extra line added in uat branch ");
     }
     
     
