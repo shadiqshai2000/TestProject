8528d3e8fdaa6fd109bfec51a3d50872b182cff8
shadiqshaikh2000 <shadiq.shaik@javis.ai>
2023-09-26 21:28:34 +0530

    removed sout

--- Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
+++ Test_Cron/src/main/java/com/example/Test_Cron/HoldCron.java
@@ -4,7 +4,6 @@ public class HoldCron {
 
     public static void main(){
         System.out.println("This is HoldCron of shadiqshaikiit id");
-        System.out.println("This is Sub branch of main");
     }
     
     
