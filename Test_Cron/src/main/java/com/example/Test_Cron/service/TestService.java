6cbcae41ea28c8cfa997c5d70834e2edb6b241e0
shadiqshaikh2000 <shadiq.shaik@javis.ai>
2023-09-27 10:47:23 +0530

    commit-6-uat

--- Test_Cron/src/main/java/com/example/Test_Cron/service/TestService.java
+++ Test_Cron/src/main/java/com/example/Test_Cron/service/TestService.java
@@ -10,7 +10,6 @@ public class TestService {
     }
 
     public static void main(){
-        System.out.println("This is a commit of main branch");
-        System.out.println("This is an extra commit");
+        System.out.println("This is a commit of uat branch");
     }
 }
